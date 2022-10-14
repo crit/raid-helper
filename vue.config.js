3d17428e9523e4dc260314e926cdb3e1d8e84603
Peter Olds <peter@olds.co>
2022-10-14 06:56:38 +0000

    feat: add gitpod support for in-browser editing

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: "all",
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    }
+  },
   transpileDependencies: true,
   publicPath: ''
 })
